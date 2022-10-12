0dca57b3b5ae421da9a7a02e39abf7cd34e859ba
Your Name <you@example.com>
2022-10-12 12:30:22 -0300

    commit inicial

---
+++ src/main/java/com/example/demo/CursospringfullApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringfullApplication.class, args);
+	}
+
+}
